c32536fbed3c9b00565b353392fe8b461a28a605
Bistoon Hosseini <46756296+bistoonh@users.noreply.github.com>
2025-09-16 03:10:42 +0330

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="snp",
-    version="0.2.2",
+    version="0.2.4",
     author="Bistoon Hosseini",
     author_email="bistoon.hosseini@gmail.com",
     description="Stepwise Noise Peeling for Nadaraya-Watson Regression",
